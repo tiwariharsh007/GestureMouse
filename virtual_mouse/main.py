eaa46f3f7eed36b3c7f82d63b0c161212cd82c97
tiwariharsh007 <tiwari007harsh@gmail.com>
2025-06-30 14:34:45 +0530

    Capturing video.

---
+++ virtual_mouse/main.py
@@ -0,0 +1,23 @@
+import cv2
+
+def main():
+    cap = cv2.VideoCapture(0)
+
+    try:
+        while cap.isOpened():
+            ret, frame = cap.read()
+
+            if not ret:
+                print("Failed to grab frame")
+                break
+
+            cv2.imshow("Gesture Mouse", frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+    finally:
+        cap.release()
+        cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
