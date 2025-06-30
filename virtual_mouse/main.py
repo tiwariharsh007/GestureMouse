ae7177d269274a08b6052877d8c0e3c5eec7cfc4
tiwariharsh007 <tiwari007harsh@gmail.com>
2025-06-30 14:46:57 +0530

    Detecting Landmarks.

--- virtual_mouse/main.py
+++ virtual_mouse/main.py
@@ -1,7 +1,20 @@
 import cv2
+import mediapipe as mp
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands(
+    static_image_mode=False,
+    model_complexity=1,
+    max_num_hands=1,
+    min_detection_confidence=0.7,
+    min_tracking_confidence=0.7
+)
+
+
 
 def main():
     cap = cv2.VideoCapture(0)
+    draw = mp.solutions.drawing_utils
 
     try:
         while cap.isOpened():
@@ -10,6 +23,21 @@ def main():
             if not ret:
                 print("Failed to grab frame")
                 break
+
+            frame = cv2.flip(frame, 1)
+            frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            processed = hands.process(frameRGB)
+
+            landmark_list = []
+
+            if processed.multi_hand_landmarks:
+                hand_landmarks = processed.multi_hand_landmarks[0]
+                draw.draw_landmarks(frame, hand_landmarks, mpHands.HAND_CONNECTIONS)
+
+                for lm in hand_landmarks.landmark:
+                    landmark_list.append((lm.x, lm.y))
+
+                print("Landmarks:", landmark_list)
 
             cv2.imshow("Gesture Mouse", frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
